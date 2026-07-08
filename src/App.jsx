94e6f0a97d992920d0a4e72679a6a63afee14cb4
rylander925 <rylander925@gmail.com>
2026-07-08 14:04:39 -0700

    Imported & Incorporated components into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import Body from './components/Body/Body.jsx'
 import NextSteps from './components/Menu/NextSteps.jsx'
+import Grass from './components/Body/Grass/Grass.jsx'
+import Background from './components/Body/Background/Background.jsx'
+import WeatherUI from './components/Menu/WeatherUI/WeatherUI.jsx'
+import Timer from './components/Menu/Timer/Timer.jsx'
 import './App.css'
 
 function App() {
   return (
     <>
       <Body />
+      <Grass />
+      <Background />
+      <WeatherUI />
+      <Timer />
 
       <div className="ticks"></div>
 
