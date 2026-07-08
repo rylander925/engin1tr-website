60d951c2884df59c0f3e7d3502fc10246961c4e3
rylander925 <rylander925@gmail.com>
2026-07-08 13:51:59 -0700

    Added grass files

---
+++ src/components/Body/Grass/Grass.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './Grass.css'
+
+function Grass() {
+  return (
+    <section id="center">
+      <div>grass</div>
+    </section>
+  )
+}
+
+export default Grass
