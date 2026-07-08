b317c3e09995802207c27540cfc8e86bdacc925a
LoadingSomething <143251849+LoadingSomething@users.noreply.github.com>
2026-07-08 15:26:20 -0700

    Example API test

--- src/components/Menu/WeatherUI/WeatherUI.jsx
+++ src/components/Menu/WeatherUI/WeatherUI.jsx
@@ -2,13 +2,26 @@ import { useState } from 'react'
 import './WeatherUI.css'
 
 function WeatherUI() {
+  const [zipCode, setZipCode] = useState(0)
+  const [city, setCity] = useState("")
+  
+  const handleButton = async () => {
+    const url = "https://api.zippopotam.us/us/"
+    try {
+      const response = await (await fetch(url + zipCode)).json()
+      setCity(response["places"][0]["place name"])
+    }
+    catch (e) {
+      console.log(e.message)
+    }
+  }
+
   return (
     <>
       <div className='menu-container'>
-        <input type='text' />
-        <button>Confirm</button>
-        <p>Text</p>
-        <p>Text2</p>
+        <input value={zipCode} onChange={e => setZipCode(e.target.value)} />
+        <button onClick={handleButton}>Confirm</button>
+        <p>{city}</p>
       </div>
     </>
   )
