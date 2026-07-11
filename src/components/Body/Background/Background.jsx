30911cd8b571d0b5304aafdd8e5bdf218398f3d1
shp11-web <shreyap11@g.ucla.edu>
2026-07-11 16:40:58 -0700

    gradients are polished, hue for grass is changed to match time of day or stormy weather, and the gradients are updated to change according to the time of day

--- src/components/Body/Background/Background.jsx
+++ src/components/Body/Background/Background.jsx
@@ -5,16 +5,15 @@ import './Background.css'
 function Background({children})
 {
   const conditions = useConditions()
-  const time = conditions?.weather?.hour
-
   const weatherData = conditions?.weather; 
+
   let timeRange = 'day'; 
   let weatherState = 'clear'; 
 
-  if (weatherData && weatherData.time)
+  if (weatherData)
   { 
-    const dateObj = new Date(weatherData.time); 
-    const hour = dateObj.getHours(); 
+    const hour = weatherData.hour;
+
     if ((hour >= 5 && hour < 7) || (hour >= 17 && hour < 19))
     { 
       timeRange = 'golden-hour'; 
@@ -23,12 +22,22 @@ function Background({children})
     { 
       timeRange = 'night'; 
     } 
-    const isStormy = weatherData.precipitation > 0 || weatherData.cloud_cover > 50; 
+
+    const isStormy = weatherData.precipitation > 0 || weatherData.cloudCover > 0.5; 
     weatherState = isStormy ? 'storm' : 'clear'; 
   }
 
   return ( 
-    <section id="center" className="sky-container" data-time={timeRange} data-weather={weatherState}> 
+    <section
+      id="center"
+      className="sky-container"
+      data-time={timeRange}
+      data-weather={weatherState}
+      style=
+      {{
+      transition: '--sky-1 2s ease, --sky-2 2s ease, --sky-3 2s ease, --sky-4 2s ease'
+      }}
+      > 
       {children} 
     </section> 
   )
