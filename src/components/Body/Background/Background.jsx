678243ec80491ea0bda86a7434f26496f01d7fd6
shp11-web <shreyap11@g.ucla.edu>
2026-07-11 13:30:25 -0700

    All gradients updated along with default background

--- src/components/Body/Background/Background.jsx
+++ src/components/Body/Background/Background.jsx
@@ -3,8 +3,10 @@ import './Background.css'
 
 function Background() {
   return (
-    <section id="center">
-      <div>Background</div>
+    <section id="center" className="sky-container">
+      <div style={{ padding: '20px', color: '#2c3e50', fontFamily: 'sans-serif', fontWeight: 'bold' }}>
+        Sky Gradient Layer Active
+      </div>
     </section>
   )
 }
