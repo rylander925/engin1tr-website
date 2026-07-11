4abfc108fdae77cebb8073b372d567cba2ba40c4
rylander925 <rylander925@gmail.com>
2026-07-11 14:03:57 -0700

    Imported conditionscontext to background.jsx

--- src/components/Body/Background/Background.jsx
+++ src/components/Body/Background/Background.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
+import { useConditions } from '../../../ConditionsContext'
 import './Background.css'
 
 function Background({children}) {
+  const conditions = useConditions()
+  const time = conditions.weather.hour
+  
   return (
     <section id="center" className="sky-container">
       <div style={{ padding: '20px', color: '#2c3e50', fontFamily: 'sans-serif', fontWeight: 'bold' }}>
