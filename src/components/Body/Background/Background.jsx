9d8b67591796ce6b557e3ba0ea7bc88ac52ab021
rylander925 <rylander925@gmail.com>
2026-07-11 14:18:44 -0700

    Fixed background formatting. Also added some possibly redundant inset and z-index attributes to sky-container

--- src/components/Body/Background/Background.jsx
+++ src/components/Body/Background/Background.jsx
@@ -8,10 +8,7 @@ function Background({children}) {
   
   return (
     <section id="center" className="sky-container">
-      <div style={{ padding: '20px', color: '#2c3e50', fontFamily: 'sans-serif', fontWeight: 'bold' }}>
-        Sky Gradient Layer Active
-        {children}
-      </div>
+      {children}
     </section>
   )
 }
