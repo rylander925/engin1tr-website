58de77d4644e8cbca64bc1879731c32510f97ba0
rylander925 <rylander925@gmail.com>
2026-07-15 14:47:39 -0700

    Adding new separate context for elapsed time

--- src/ElapsedTimeContext.jsx
+++ src/ElapsedTimeContext.jsx
@@ -3,6 +3,32 @@ import { createContext, useContext, useDispatch } from 'react'
 const ElapsedTimeContext = createContext(null);
 const ElapsedTimeDispatchContext = createContext(null);
 
-export default function ElapsedTimeContextProvider() {
-    
+const initialTime = 0
+
+export function ElapsedTimeContextProvider({children}) {
+    const [elapsedTime, elapsedTimeDispatch] = useDispatch(elapsedTimeReducer, initialTime);
+
+    return(
+    <ElapsedTimeContext value={elapsedTime}>
+        <ElapsedTimeDispatchContext value={elapsedTimeDispatch}>
+            {children}
+        </ElapsedTimeDispatchContext>
+    </ElapsedTimeContext>
+    );
 } 
+
+export function useElapsedTime() {
+    return useContext(ElapsedTimeContext);
+}
+
+export function useElapsedTimeDispatch() {
+    return useContext(ElapsedTimeDispatchContext);
+}
+
+function elapsedTimeReducer(elapsedTime, action) {
+    switch(action.type) {
+        case('increment-time'): {
+            return 
+        }
+    }
+}
